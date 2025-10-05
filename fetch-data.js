819658743bebab8dc9350ecb48f004b01a85c395
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-10-05 16:27:44 +0200

    Modified files

--- fetch-data.js
+++ fetch-data.js
@@ -1,5 +1,5 @@
 const fetchUserData = async () => {
-    const apiurl = 'https://jsonplaceholder.typicode.com/users';
+    const apiurl = 'https://jsonplaceholder.typicode.com/users'
     try{
         const response = await fetch(apiurl);
         const users = await response.json();
@@ -8,7 +8,7 @@ const fetchUserData = async () => {
         const userList = <ul></ul>;
         users.array.forEach(userList => {
             const list = <li></li>;
-            list.textcon
+            
 
         });
     }
